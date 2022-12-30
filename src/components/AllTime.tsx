a217dfb0288b7b9a7e00eef7ea4a12fff6a5ab12
Jouni12 <jouni.kotilainen12@gmail.com>
2022-12-30 10:52:08 +0000

    Add missing views. Add latest game data.

--- src/components/AllTime.tsx
+++ src/components/AllTime.tsx
@@ -1,4 +1,5 @@
 import React, { FC } from "react";
+import allTime from '../data/all_time_stats.json';
 
 const AllTime: FC = () => {
 
@@ -6,7 +7,32 @@ const AllTime: FC = () => {
 
     return (
         <>
-            <span>Alltime!</span>
+            <table id="allTimeStats" className="table table-striped table-bordered" width="100%">
+                <thead>
+                    <tr>
+                        <th></th>
+                        <th>Nimi</th>
+                        <th>Maalit</th>
+                        <th>Syötöt</th>
+                        <th>Pisteet</th>
+                        <th title="Mukana vain 2. divari jäähyt"> Jäähyt (min)</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {allTime.roster.map((player: any) => {
+                        return (
+                            <tr>
+                                <td>{player.rank}</td>
+                                <td>{player.name}</td>
+                                <td>{player.goals}</td>
+                                <td>{player.assists}</td>
+                                <td>{player.points}</td>
+                                <td>{player.penalties}</td>
+                            </tr>
+                        )
+                    })}
+                </tbody>
+            </table>
         </>
     );
 };
